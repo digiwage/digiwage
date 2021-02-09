5b545440fcab5337a9e08b8ec4b1f1e33ee93aa0
gdiscord <gnetbusy@gmail.com>
2021-02-09 23:37:15 +0000

    changed protocol and zerocoin start height

--- src/version.h
+++ src/version.h
@@ -11,7 +11,7 @@
  * network protocol versioning
  */
 
-static const int PROTOCOL_VERSION = 70810;
+static const int PROTOCOL_VERSION = 80810;
 
 //! initial proto version, to be increased after version/verack negotiation
 static const int INIT_PROTO_VERSION = 209;
@@ -21,10 +21,10 @@ static const int GETHEADERS_VERSION = 70077;
 
 //! disconnect from peers older than this proto version
 static const int MIN_PEER_PROTO_VERSION_BEFORE_ENFORCEMENT = 70710;
-static const int MIN_PEER_PROTO_VERSION_AFTER_ENFORCEMENT = 70810;
+static const int MIN_PEER_PROTO_VERSION_AFTER_ENFORCEMENT = 80810;
 
 //! masternodes older than this proto version use old strMessage format for mnannounce
-static const int MIN_PEER_MNANNOUNCE = 70810;
+static const int MIN_PEER_MNANNOUNCE = 80810;
 
 //! nTime field added to CAddress, starting with this version;
 //! if possible, avoid requesting addresses nodes older than this
