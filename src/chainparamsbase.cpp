19b6ef36af1dc38b4320a79f16f8710487d15ab5
digiwage <37214252+digiwage@users.noreply.github.com>
2021-02-20 11:50:37 -0600

    Update chainparamsbase.cpp

--- src/chainparamsbase.cpp
+++ src/chainparamsbase.cpp
@@ -22,7 +22,7 @@ public:
     CBaseMainParams()
     {
         networkID = CBaseChainParams::MAIN;
-        nRPCPort = 46002;
+        nRPCPort = 46008;
     }
 };
 static CBaseMainParams mainParams;
