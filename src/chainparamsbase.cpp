bf20a6a6fe3355534e3eced61c7f5c8556f3c096
digiwage <37214252+digiwage@users.noreply.github.com>
2021-02-20 11:51:44 -0600

    revert

--- src/chainparamsbase.cpp
+++ src/chainparamsbase.cpp
@@ -22,7 +22,7 @@ public:
     CBaseMainParams()
     {
         networkID = CBaseChainParams::MAIN;
-        nRPCPort = 46008;
+        nRPCPort = 46002;
     }
 };
 static CBaseMainParams mainParams;
